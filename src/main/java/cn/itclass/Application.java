c957e36b4e5ebda47ea21e619ed0b43a3998e403
Yi Chen <847448470@qq.com>
2018-09-13 17:20:45 +0800

    change Application

--- src/main/java/cn/itclass/Application.java
+++ src/main/java/cn/itclass/Application.java
@@ -3,17 +3,12 @@ package cn.itclass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
-@ComponentScan(basePackages="cn.itclass")
-public class Application extends SpringBootServletInitializer {
+@SpringBootApplication
+public class Application {
     private static Logger logger = LoggerFactory.getLogger(Application.class);
 
     /**

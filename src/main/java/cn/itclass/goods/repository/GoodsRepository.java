5a68019c03a3655696eb11da0f129e02cbe4d46d
Yi Chen <847448470@qq.com>
2018-09-13 17:25:00 +0800

    create GoodsRepository

--- src/main/java/cn/itclass/goods/repository/GoodsRepository.java
+++ src/main/java/cn/itclass/goods/repository/GoodsRepository.java
@@ -1,4 +1,24 @@
 package cn.itclass.goods.repository;
 
-public class GoodsRepository {
+import cn.itclass.goods.entity.GoodsInfoEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * 商品信息表
+ *
+ * @author Yi Chen
+ *
+ */
+
+public interface GoodsRepository extends JpaRepository<GoodsInfoEntity, Long> {
+
+//    @Query("select d from GoodsInfoEntity d")
+//    Page<GoodsInfoEntity> findAllGoods(Pageable pageable);
+
+//    @Override
+//    Page<GoodsInfoEntity> findAll(Pageable pageable);
 }

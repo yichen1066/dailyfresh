86e56d9aba6bc54fa7da675bc3234683a9409beb
Yi Chen <847448470@qq.com>
2018-09-13 17:24:41 +0800

    create SystemProperties

---
+++ src/main/java/cn/itclass/goods/entity/SystemProperties.java
@@ -0,0 +1,23 @@
+package cn.itclass.goods.entity;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * 系统配置文件
+ */
+@Component
+@ConfigurationProperties(prefix = "df.goods")
+@PropertySource(value = "system.properties")
+public class SystemProperties {
+    private Integer pageSize;
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
